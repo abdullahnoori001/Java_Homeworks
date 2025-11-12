2c181443cc7872edd333b24b3e64ca3cf8348e82
abdullahnoori001 <noori.2004.abdullah@gmail.com>
2025-11-12 00:34:23 -0800

    Add files via upload

---
+++ Aria/Aria.java
@@ -0,0 +1,22 @@
+package aria;
+import java.util.Scanner;
+
+//Auther :abdullah noori
+public class Aria {}
+
+public class Aria {
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        double Aria;
+
+        System.out.println("Enter a value for raduis");
+        double raduis = input.nextDouble();
+
+        area = raduis * raduis * 3.14;
+
+        System.out.println("The area for the circle of raduis\t" +raduis+"\tis\t"+area);
