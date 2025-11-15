abcad17488e074eab063cd1076b6b6ea9b500a75
abdullahnoori001 <noori.2004.abdullah@gmail.com>
2025-11-14 21:03:40 -0800

    Add files via upload

--- CalculateInterest.java
+++ CalculateInterest.java
@@ -1,16 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.mycompany.muhamad;
+package com.mycompany.muhammad;
 
-/**
- *
- * @author USER
- */
-public class Muhamad {
+import java.util.Scanner;
 
+public class CalculateInterest {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your balance in  bank ");
+        double balance = input.nextDouble();
+
+        System.out.println("What is the annualInterestRate ");
+        double annualInterestRate = input.nextDouble();
+
+        double interest = balance*(annualInterestRate/1200);
+        System.out.println("The interest is = "+interest);
     }
-}
+
+    
