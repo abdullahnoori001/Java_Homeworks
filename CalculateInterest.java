ac8fd63b37eff9bc079daf3219cc839878f09c2d
abdullahnoori001 <noori.2004.abdullah@gmail.com>
2025-11-14 20:54:42 -0800

    Add files via upload

---
+++ CalculateInterest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.muhamad;
+
+/**
+ *
+ * @author USER
+ */
+public class Muhamad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
